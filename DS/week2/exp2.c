5b19035166c060c6f866b897caf9fb6ae6ea1533
Aaradhya Raut <aaradhya1426@gmail.com>
2025-07-19 05:11:40 +0000

    end of 19th july

--- DS/week2/exp2.c
+++ DS/week2/exp2.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+
 #define MAX_SIZE 100
 
 int stackTop = -1;
+char stack[100];
 
+push(char c) {
+    if(stackTop == MAX_SIZE - 1) {
+        printf("Stack Overflow! Cannot push %c\n", c);
+    } else {
+        stackTop++;
+        stack[stackTop] = c;
+    }
+}
+
+char pop() {
+    if (stackTop < 0) {
+        return '\0';
+    } else {
+        char poppedChar = stack[stackTop];
+        stackTop--;
+        return poppedChar;
+    }
+}
+
+void getPrecendence() {
+    
+}
 
 int main(void) {
 
+    printf("\n");
+    return 0;
 }
